c5327798201e039f86757329d6b0f688b0b7dcdc
Christopher Peterson <crpete23@gmail.com>
2018-09-17 13:05:22 -0700

    updated response for login/sign in to only send back userId, created new userVerify route

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,12 +1,14 @@
 const model = require('../models/users')
 const auth = require('../lib/auth')
+const {parseToken} = require('../lib/auth')
 
 async function signup (req, res, next) {
   try {
     const response = await model.create(req.body)
     const token = auth.createToken(response.id)
+    const userId = response.id
 
-    res.status(201).json({ response, token })
+    res.status(201).json({ userId, token })
   } catch (e) {
     next({ status: 400, error: `User could not be registered` })
   }
@@ -16,13 +18,26 @@ async function login (req, res, next) {
   try {
     const response = await model.login(req.body)
     const token = auth.createToken(response.id)
+    const userId = response.id
 
-    res.json({ response, token })
+    res.json({ userId, token })
   } catch (e) {
     next({ status: 401, error: `Email or password is incorrect` })
   }
 }
 
+async function verify(req, res, next){
+  try{
+    const token = parseToken(req.headers.authorization)
+    const userId = token.sub.id
+
+    res.json({ userId })
+  } catch (e){
+    console.log(e)
+    next({ status: 400, error: 'Token is invalid or expired'})
+  }
+}
+
 module.exports = {
-  signup, login
+  signup, login, verify
 }
