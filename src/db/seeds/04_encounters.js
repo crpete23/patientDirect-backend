516e7fa55a12778520f96eea5889c0a433cb08a9
Christopher Peterson <crpete23@gmail.com>
2018-09-19 15:55:41 -0700

    fixed errors on seeds

--- src/db/seeds/04_encounters.js
+++ src/db/seeds/04_encounters.js
@@ -71,11 +71,11 @@ exports.seed = function (knex, Promise) {
     {id:3, patient_id: 3, doctor_id: 1, date: '2018/08/30', time: '4:00 pm', hx:{}
     },
     {id:4, patient_id: 1, doctor_id: 1, date: '2018/09/19', time: '4:00 pm', hx:{}
-    }
+    },
     {id:5, patient_id: 1, doctor_id: 1, date: '2018/09/20', time: '4:00 pm', hx:{}
-    }
+    },
     {id:6, patient_id: 1, doctor_id: 1, date: '2018/09/21', time: '4:00 pm', hx:{}
-    }
+    },
     {id:7, patient_id: 1, doctor_id: 1, date: '2018/09/22', time: '4:00 pm', hx:{}
     }
   ])
