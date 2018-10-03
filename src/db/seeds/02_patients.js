0f6a35dc197fad4d9b0c58786affa5413fcab6a2
Christopher Peterson <crpete23@gmail.com>
2018-10-02 19:55:12 -0700

    added new patients to seed data, added encounters

--- src/db/seeds/02_patients.js
+++ src/db/seeds/02_patients.js
@@ -7,7 +7,12 @@ exports.seed = function (knex, Promise) {
       return knex('patients').insert([
         {id: 1, first_name: 'chris', last_name: 'peterson', dob: '1992/04/12', sex: 'male'},
         {id: 2, first_name: 'maria', last_name: 'peterson', dob: '1998/05/19', sex: 'female'},
-        {id: 3, first_name: 'erik', last_name: 'peterson', dob: '1996/09/27', sex: 'male'}
+        {id: 3, first_name: 'erik', last_name: 'peterson', dob: '1996/09/27', sex: 'male'},
+        {id: 4, first_name: 'hermione', last_name: 'granger', dob: '1979/09/19', sex: 'female'},
+        {id: 5, first_name: 'ron', last_name: 'weasley', dob: '1980/03/01', sex: 'male'},
+        {id: 6, first_name: 'harry', last_name: 'potter', dob: '1980/07/31', sex: 'male'},
+        {id: 7, first_name: 'tom', last_name: 'riddle', dob: '1926/12/31', sex: 'male'},
+        {id: 8, first_name: 'albus', last_name: 'dumbledore', dob: '1881/12/31', sex: 'male'}
       ])
     })
     .then(function () {
--- src/db/seeds/04_encounters.js
+++ src/db/seeds/04_encounters.js
@@ -6,7 +6,83 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('encounters').insert([
-    {id:1, patient_id: 1, doctor_id: 1, date: '2018/10/02', time: '11:00 am', hx:{}
+    {id:1, patient_id: 1, doctor_id: 1, date: '2018/10/02', time: '11:00 am', "hx": {
+            "hpi": {
+                "cc": [
+                    "chest pain"
+                ],
+                "quality": "sharp",
+                "timing": "intermittent",
+                "severity": "10",
+                "duration": "several weeks",
+                "location": "central chest",
+                "alleviating factors": [
+                    "nitroglycerin"
+                ],
+                "exacerbating factors": [
+                    "exertion"
+                ],
+                "associated symptoms": [
+                    "shortness of breath",
+                    "diaphoresis (sweating)",
+                    "nausea",
+                    "vomitting",
+                    "fatigue",
+                    "dizziness/light headedness"
+                ],
+                "context": "Whenever I overexert"
+            },
+            "ros": {
+                "Constitutional": {
+                    "fatigue": true,
+                    "fever": false,
+                    "unintentional weight loss": false
+                },
+                "Eyes": {
+                    "eye pain": false,
+                    "blurry vision": false
+                },
+                "ENT": {
+                    "sore throat": false,
+                    "epistaxis": false
+                },
+                "Cardiovascular": {
+                    "chest pain": true,
+                    "palpitations": false,
+                    "dyspnea": true
+                },
+                "Respiratory": {
+                    "shortness of breath": true,
+                    "cough": false
+                },
+                "Gastrointestinal": {
+                    "nausea": true,
+                    "vomiting": false,
+                    "hematochezia": false,
+                    "melena": false
+                },
+                "Genitourinary": {
+                    "hematuria": false
+                },
+                "Musculoskeletal": {
+                    "myalgias": false,
+                    "joint pain": false
+                },
+                "Neurological": {
+                    "history of CVA": false,
+                    "history of TIA": false,
+                    "headache": false,
+                    "lightheadedness": true,
+                    "syncope": false
+                },
+                "Endocrine": {
+                    "diabetes": true
+                },
+                "Hematologic": {
+                    "anticoagulant use": false
+                }
+            }
+        }
     },
     {id:2, patient_id: 2, doctor_id: 1, date: '2018/10/02', time: '10:00 am', hx:{}
     },
@@ -30,6 +106,16 @@ exports.seed = function (knex, Promise) {
     },
     {id:12, patient_id: 1, doctor_id: 1, date: '2018/10/11', time: '4:00 pm', hx:{}
     },
+    {id:13, patient_id: 4, doctor_id: 1, date: '2018/10/02', time: '11:00 am', hx:{}
+    },
+    {id:14, patient_id: 5, doctor_id: 1, date: '2018/10/02', time: '12:00 pm', hx:{}
+    },
+    {id:15, patient_id: 6, doctor_id: 1, date: '2018/10/02', time: '1:00 pm', hx:{}
+    },
+    {id:16, patient_id: 7, doctor_id: 1, date: '2018/10/02', time: '2:00 pm', hx:{}
+    },
+    {id:17, patient_id: 8, doctor_id: 1, date: '2018/10/02', time: '3:00 pm', hx:{}
+    }
   ])
     })
     .then(function () {
