daf13a8f9ad39b7f87f9c1ee7401c60c1b43d23f
Christopher Peterson <crpete23@gmail.com>
2018-10-23 11:28:38 -0700

    added server side auth validation

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -5,13 +5,18 @@ const {parseToken} = require('../lib/auth')
 
 async function signup (req, res, next) {
   try {
-    const response = await model.create(req.body)
+    const newUser = req.body
+    if(!newUser.doc_first_name || !newUser.doc_last_name || !newUser.email || !newUser.password ) {
+      throw new Error("Missing User Signup Information")
+    }
+
+    const response = await model.create(newUser)
     const token = auth.createToken(response.id)
     const userId = response.id
 
     await tempModel.newUserHpiTemps(userId)
     await tempModel.newUserRosTemp(userId)
-    
+
     res.status(201).json({ userId, token })
   } catch (e) {
     console.log(e)
@@ -21,6 +26,11 @@ async function signup (req, res, next) {
 
 async function login (req, res, next) {
   try {
+    const user = req.body
+    if(!user.email || !user.password) {
+      throw new Error("Missing User Login Information")
+    }
+
     const response = await model.login(req.body)
     const token = auth.createToken(response.id)
     const userId = response.id
